5b751a8ca3028d64ccfe271fb8ec5fb0437193f1
zhao <1249632746@qq.com>
2024-04-09 23:35:02 +0800

    文章列表样式调整

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,24 +1,38 @@
 import {Link, useNavigate} from 'react-router-dom'
-import {Card, Breadcrumb, Form, Button, Radio, DatePicker, Select, Popconfirm} from 'antd'
-// 引入汉化包 时间选择器显示中文
-import locale from 'antd/es/date-picker/locale/zh_CN'
-
-// 导入资源
+import {Card, Breadcrumb, Form, Button, Radio, DatePicker, Select, Popconfirm, Row, Col} from 'antd'
 import {Table, Tag, Space} from 'antd'
 import {EditOutlined, DeleteOutlined} from '@ant-design/icons'
 import img404 from '@/assets/error.png'
 import {useChannel} from '@/hooks/useChannel'
 import {useEffect, useState} from 'react'
 import {delArticleAPI, getArticleListAPI} from '@/apis/article'
+import styled from "styled-components";
 
 const {Option} = Select
 const {RangePicker} = DatePicker
 
+const BreadCrumbContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    min-height: 56px;
+    color: rgba(0, 0, 0, 0.88);
+    font-weight: 600;
+    font-size: 16px;
+`
+const MyForm = styled(Form)`
+
+`
+const MyTable = styled(Table)`
+    .ant-table-body {
+        height: calc(100vh - 360px);
+    }
+`
+
+
 const Article = () => {
     const navigate = useNavigate()
     const {channelList} = useChannel()
-    // 准备列数据
-    // 定义状态枚举
     const status = {
         1: <Tag color='warning'>待审核</Tag>,
         2: <Tag color='success'>审核通过</Tag>,
@@ -40,9 +54,6 @@ const Article = () => {
         {
             title: '状态',
             dataIndex: 'status',
-            // data - 后端返回的状态status 根据它做条件渲染
-            // data === 1 => 待审核
-            // data === 2 => 审核通过
             render: data => status[data]
         },
         {
@@ -88,8 +99,6 @@ const Article = () => {
         }
     ]
 
-    // 筛选功能
-    // 1. 准备参数
     const [reqData, setReqData] = useState({
         status: '',
         channel_id: '',
@@ -113,10 +122,7 @@ const Article = () => {
     }, [reqData])
 
 
-    // 2. 获取筛选数据
     const onFinish = (formValue) => {
-        console.log(formValue)
-        // 3. 把表单收集到数据放到参数中(不可变的方式)
         setReqData({
             ...reqData,
             channel_id: formValue.channel_id,
@@ -124,14 +130,10 @@ const Article = () => {
             begin_pubdate: formValue.date[0].format('YYYY-MM-DD'),
             end_pubdate: formValue.date[1].format('YYYY-MM-DD')
         })
-        // 4. 重新拉取文章列表 + 渲染table逻辑重复的 - 复用
-        // reqData依赖项发生变化 重复执行副作用函数
     }
 
     // 分页
     const onPageChange = (page) => {
-        console.log(page)
-        // 修改参数依赖项 引发数据的重新获取列表渲染
         setReqData({
             ...reqData,
             page
@@ -147,54 +149,58 @@ const Article = () => {
     }
 
     return (
-        <div>
-            <Card
-                title={
-                    <Breadcrumb items={[
+        <div style={{padding: '0 24px', height: '100%'}}>
+            <BreadCrumbContainer>
+                <Breadcrumb
+                    items={[
                         {title: <Link to={'/'}>首页</Link>},
                         {title: '文章列表'},
-                    ]}/>
-                }
-                style={{marginBottom: 20}}
-            >
-                <Form initialValues={{status: ''}} onFinish={onFinish}>
-                    <Form.Item label="状态" name="status">
-                        <Radio.Group>
-                            <Radio value={''}>全部</Radio>
-                            <Radio value={1}>待审核</Radio>
-                            <Radio value={2}>审核通过</Radio>
-                        </Radio.Group>
-                    </Form.Item>
+                    ]}
+                    style={{margin: '10px 0'}}
+                />
+            </BreadCrumbContainer>
 
-                    <Form.Item label="频道" name="channel_id">
-                        <Select
-                            placeholder="请选择文章频道"
-                            style={{width: 120}}
-                        >
-                            {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}
-                        </Select>
-                    </Form.Item>
+            <MyForm layout="inline" initialValues={{status: ''}} onFinish={onFinish}>
+                <Form.Item label="状态" name="status">
+                    <Radio.Group>
+                        <Radio value={''}>全部</Radio>
+                        <Radio value={1}>待审核</Radio>
+                        <Radio value={2}>审核通过</Radio>
+                    </Radio.Group>
+                </Form.Item>
 
-                    <Form.Item label="日期" name="date">
-                        {/* 传入locale属性 控制中文显示*/}
-                        <RangePicker locale={locale}></RangePicker>
-                    </Form.Item>
+                <Form.Item label="频道" name="channel_id">
+                    <Select
+                        placeholder="请选择文章频道"
+                        allowClear={true}
+                        style={{minWidth: '230px'}}
+                    >
+                        {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}
+                    </Select>
+                </Form.Item>
 
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" style={{marginLeft: 40}}>
-                            筛选
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
-            {/* 表格区域 */}
-            <Card title={`根据筛选条件共查询到 ${count} 条结果：`}>
-                <Table rowKey="id" columns={columns} dataSource={list} pagination={{
+                <Form.Item label="日期" name="date">
+                    <RangePicker/>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        筛选
+                    </Button>
+                </Form.Item>
+            </MyForm>
+
+            <MyTable
+                style={{marginTop: '20px'}} rowKey="id" columns={columns}
+                dataSource={list}
+                pagination={{
                     total: count,
                     pageSize: reqData.per_page,
                     onChange: onPageChange
-                }}/>
-            </Card>
+                }}
+                scroll={{y: "calc(100vh - 360px)"}}
+            />
+
         </div>
     )
 }
